fbee9f94af1de637af27662d4c8cd47cf9a919f2
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-04-28 21:26:31 -0300

    Create ImportJframeClassMainMethod.java

---
+++ Grafos-Java/ImportJframeClassMainMethod.java
@@ -0,0 +1,10 @@
+import projeto.novatela.ntela.Cadastrar;
+
+
+public class Ntela {
+    public static void main(String[] args) {
+        Cadastrar telaCadastro = new Cadastrar();
+        telaCadastro.setVisible(true);
+        // Outras operações com a tela de cadastro, se necessário
+    }
+}
