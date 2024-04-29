d72ac6c968bd516f97a9472e4ac96317e763501d
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-04-29 14:43:31 -0300

    Add files via upload

---
+++ Grafos-Java/PrejetoTransmissão/Transmissão/Transmissao/src/main/java/projeto/transmissao/Transmissao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package projeto.transmissao;
+
+/**
+ *
+ * @author usuario
+ */
+public class Transmissao {
+
+    public static void main(String[] args) {
+        new Janela1().setVisible(true);
+    }
+}
