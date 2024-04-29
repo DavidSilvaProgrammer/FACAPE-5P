32fbd2fc0f99a83845804a3952db66f3f3e4c15e
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-04-28 21:38:49 -0300

    Add files via upload

---
+++ Grafos-Java/NovaTela/Ntela/src/main/java/projeto/novatela/ntela/Ntela.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package projeto.novatela.ntela;
+
+/**
+ *
+ * @author usuario
+ */
+public class Ntela {
+
+    public static void main(String[] args) {
+        
+        Tela1 minhaTela1 = new Tela1();
+        minhaTela1.setVisible(true);
+    }
+}
