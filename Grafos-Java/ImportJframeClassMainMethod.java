4bf445c9772169f7ab58c7d6b47932ee84fd3ff9
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-04-28 21:31:02 -0300

    Update ImportJframeClassMainMethod.java

--- Grafos-Java/ImportJframeClassMainMethod.java
+++ Grafos-Java/ImportJframeClassMainMethod.java
@@ -8,3 +8,26 @@ public class Ntela {
         // Outras operações com a tela de cadastro, se necessário
     }
 }
+
+
+**********************
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package projeto.novatela.ntela;
+
+/**
+ *
+ * @author usuario
+ */
+public class Ntela {
+
+    public static void main(String[] args) {
+        
+        Cadastrar meuCadastrar = new Cadastrar();
+        meuCadastrar.setVisible(true);
+    }
+}
