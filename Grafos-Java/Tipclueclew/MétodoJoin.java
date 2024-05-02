675b64b333e630cab64bc78c34ed6babb4b0423f
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-02 08:43:41 -0300

    Create MétodoJoin.java

---
+++ Grafos-Java/Tipclueclew/MétodoJoin.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String[] vetor = {"elemento1", "elemento2", "elemento3"};
+        String resultado = String.join(", ", vetor); // Junta os elementos do vetor separados por ", "
+        System.out.println(resultado);
+
+	}
+}
