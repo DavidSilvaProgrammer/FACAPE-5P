25a2302d4343aeec0f78071eb1a49025fcde0909
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-02 09:10:59 -0300

    Update MatrizParaVariáveStringl.java

--- Grafos-Java/Tipclueclew/MatrizParaVariáveStringl.java
+++ Grafos-Java/Tipclueclew/MatrizParaVariáveStringl.java
@@ -27,3 +27,27 @@ public class Main {
         System.out.println("Matriz como string: " + resultado);
     }
 }
+
+
+/*
+
+O comando matriz[0].length retorna o comprimento da primeira linha da matriz, 
+ou seja, o número de elementos na primeira linha.
+
+Vamos explicar em detalhes:
+
+    matriz[0]: Isso acessa a primeira linha da matriz. No Java, a indexação de arrays 
+    começa em 0, então matriz[0] representa a primeira linha da matriz matriz.
+
+    matriz[0].length: Isso retorna o comprimento da primeira linha da matriz. No Java, o 
+    buto length de um array multidimensional retorna o número de elementos na dimensão especificada. 
+    No caso de um array bidimensional como matriz, matriz[0].length retorna o número de elementos 
+    na primeira dimensão, ou seja, o número de colunas na primeira linha da matriz.
+
+Então, em resumo, matriz[0].length retorna o número de colunas na primeira linha da matriz. 
+Isso é útil quando você quer saber quantas colunas a matriz tem.
+
+*/
+
+
+*/
