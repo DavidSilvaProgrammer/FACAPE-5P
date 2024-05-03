0a10e0a9bb3b12f1f20c2a66999804ede867777b
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-03 12:01:05 -0300

    Create LocalehSystemPause.c

---
+++ CompGráfica/Unid-II/BaseDevC++/LocalehSystemPause.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    int numero;
+
+    // Solicita ao usuário que digite um número inteiro
+    printf("Digite um número inteiro: ");
+    scanf("%d", &numero);
+
+    // Calcula o dobro do número digitado
+    int dobro = numero * 2;
+
+    // Imprime o dobro do número
+    printf("O dobro de %d é: %d\n", numero, dobro);
+    
+    
+    system("PAUSE");
+    return 0;
+}
