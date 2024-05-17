21854a18707e8a9f9248f2d82c263dd5bb628624
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-17 06:13:05 -0300

    Create Length()ParaString.java

---
+++ Grafos-Java/Accomplishment/Length()ParaString.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Definindo uma string com um caractere de nova linha
+        String example = "Hello\nWorld";
+        
+        // Exibir o comprimento da string
+        System.out.println("Comprimento da string: " + example.length());
+        
+        // Exibir os caracteres e seus índices
+        for (int i = 0; i < example.length(); i++) {
+            System.out.println("Índice " + i + ": " + example.charAt(i));
+            
+            
+        System.out.println(example.length());
+        }
+    }
+}
