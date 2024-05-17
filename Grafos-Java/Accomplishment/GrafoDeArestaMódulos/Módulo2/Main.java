7359f05345c34bc7294c9a3bcdd8f27ae2ea64a6
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-17 08:43:08 -0300

    Create Main.java

---
+++ Grafos-Java/Accomplishment/GrafoDeArestaMódulos/Módulo2/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] matriz = {
+                {0, 1, 0, 1},
+                {1, 0, 1, 0},
+                {0, 0, 1, 1},
+                {1, 0, 0, 0}
+        };
+
+        String matrizString = Conversora.conversorGrafoDeAresta(matriz);
+        System.out.println(matrizString);
+    }
+}
