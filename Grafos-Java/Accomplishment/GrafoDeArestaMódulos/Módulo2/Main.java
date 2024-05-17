7783711673a7770981f541409bdb793aa5926655
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-17 09:28:18 -0300

    Update Main.java

--- Grafos-Java/Accomplishment/GrafoDeArestaMódulos/Módulo2/Main.java
+++ Grafos-Java/Accomplishment/GrafoDeArestaMódulos/Módulo2/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // Define uma matriz de adjacência representando um grafo
         int[][] matriz = {
                 {0, 1, 0, 1},
                 {1, 0, 1, 0},
@@ -7,7 +8,10 @@ public class Main {
                 {1, 0, 0, 0}
         };
 
+        // Chama o método de conversão da classe Conversora para converter a matriz de adjacência em um grafo de aresta
         String matrizString = Conversora.conversorGrafoDeAresta(matriz);
+        
+        // Imprime a representação em string do grafo de aresta
         System.out.println(matrizString);
     }
 }
