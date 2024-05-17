90fef55ccf78a8d52680aefea7cdf7249224c9fa
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-17 08:00:06 -0300

    Create GrafoAresta.java

---
+++ Grafos-Java/Accomplishment/GrafoDeArestaMódulos/Módulo1/GrafoAresta.java
@@ -0,0 +1,13 @@
+public class GrafoAresta {
+    private int aresta_esquerda;
+    private int aresta_direita;
+
+    public GrafoAresta(int aresta_esquerda, int aresta_direita) {
+        this.aresta_esquerda = aresta_esquerda + 1;
+        this.aresta_direita = aresta_direita + 1;
+    }
+
+    public String getAresta() {
+        return "A(" + aresta_esquerda + "," + aresta_direita + ")";
+    }
+}
