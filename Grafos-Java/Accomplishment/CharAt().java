03d126c6489621223ffa51c0273c58d48cfedb66
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-17 05:28:38 -0300

    Create CharAt().java

---
+++ Grafos-Java/Accomplishment/CharAt().java
@@ -0,0 +1,15 @@
+if (arestaI.charAt(3) == arestaJ.charAt(3)) {
+    // Faça algo se os caracteres forem iguais
+}
+
+
+/*
+
+Se você deseja comparar apenas um caractere específico de uma string, você pode usar o método charAt(index) 
+para acessar o caractere na posição especificada pelo índice. Aqui está como você pode fazer isso:
+
+Neste exemplo, charAt(3) acessa o quarto caractere (índice 3) da string arestaI e arestaJ, e então 
+você pode comparar esses caracteres como desejar. Lembre-se de que os índices em Java começam em 0, então 
+o quarto caractere tem índice 3.
+
+*/
