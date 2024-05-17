523af1391fbe8753300f5a9ac96e641922b65393
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-17 01:25:39 -0300

    Create RetornaIndicesMatrizPorClass.java

---
+++ Grafos-Java/Accomplishment/RetornaIndicesMatrizPorClass.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class GrafoAresta {
+    private int aresta_esquerda;
+    private int aresta_direita;
+
+    public GrafoAresta(int aresta_esquerda, int aresta_direita) {
+        this.aresta_esquerda = aresta_esquerda;
+        this.aresta_direita = aresta_direita;
+    }
+
+    public String getAresta() {
+        return "A(" + aresta_esquerda + "," + aresta_direita + ")";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Matriz de exemplo
+        int[][] matriz = {
+            {0, 1, 0, 1},
+            {1, 0, 1, 0},
+            {0, 0, 1, 1},
+            {1, 0, 0, 0}
+        };
+
+        List<GrafoAresta> indicesDosUns = encontrarIndicesDosUns(matriz);
+        
+        System.out.println("Índices dos elementos que possuem o 1:");
+        for (GrafoAresta aresta : indicesDosUns) {
+            System.out.println(aresta.getAresta());
+        }
+    }
+
+    public static List<GrafoAresta> encontrarIndicesDosUns(int[][] matriz) {
+        List<GrafoAresta> indicesDosUns = new ArrayList<>();
+        int linhas = matriz.length;
+        int colunas = matriz[0].length;
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                if (matriz[i][j] == 1) {
+                    GrafoAresta aresta = new GrafoAresta(i, j);
+                    indicesDosUns.add(aresta);
+                }
+            }
+        }
+
+        return indicesDosUns;
+    }
+}
