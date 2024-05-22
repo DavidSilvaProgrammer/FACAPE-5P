c02335caefebb9e38b717645b8a39f54d6f504dd
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-22 13:31:05 -0300

    Create System.err.println().java

---
+++ Grafos-Java/CodeLodgement/LerArquivo/System.err.println().java
@@ -0,0 +1,14 @@
+System.err.println("Erro: Não foi possível abrir o arquivo!");
+
+/*
+System.err.println é usado para imprimir mensagens de erro ou informações de diagnóstico na saída de erro padrão 
+(standard error stream) do programa Java.
+
+    System.err é uma instância da classe PrintStream que representa a saída de erro padrão.
+    println() é um método da classe PrintStream que imprime uma linha de texto na saída 
+  especificada, seguida de uma quebra de linha.
+
+Ao usar System.err.println, a mensagem será exibida na saída de erro padrão, que geralmente é exibida 
+em uma cor diferente ou de forma diferenciada no console, tornando mais fácil distinguir mensagens de 
+  erro de mensagens normais de saída.
+*/
