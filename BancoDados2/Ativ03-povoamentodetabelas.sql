94d3f76efce42b3a0be57a0aa76b103121930e0c
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-23 16:55:25 -0300

    Create Ativ03-povoamentodetabelas.sql

---
+++ BancoDados2/Ativ03-povoamentodetabelas.sql
@@ -0,0 +1,91 @@
+insert into cliente values(2, 'Elon Musk', 'SpaceX HQ N. 246', 'Los Angeles', 'LA', '81
+98111-1111', '81 98111-1111');
+insert into cliente values(3, 'Steve Jobs', 'Apple Campus N. 360', 'Cupertino', 'CP', '81
+98222-2222', '81 98222-2222');
+insert into cliente values(4, 'Bill Gates', 'Microsoft HQ N. 321', 'Redmond', 'RD', '81
+98333-3333', '81 98333-3333');
+insert into cliente values(5, 'Mark Zuckerberg', 'Facebook Campus N. 153', 'Menlo Park',
+'MP', '81 98444-4444', '81 98444-4444');
+insert into cliente values(6, 'Jeff Bezos', 'Amazon HQ N. 641', 'Seattle', 'ST', '31 98555-5555',
+'31 98555-5555');
+insert into cliente values(7, 'Tim Berners-Lee', 'World Wide Web Blvd N. 133', 'London', 'LD',
+'81 98666-6666', '81 98666-6666');
+insert into cliente values(8, 'Larry Page', 'Googleplex N. 777', 'Mountain View', 'MV', '81
+98146-0272', '81 98146-0272');
+insert into cliente values(9, 'Satya Nadella', 'Microsoft HQ N. 111', 'Redmond', 'RD', '81
+98777-7777', '81 98777-7777');
+insert into cliente values(10, 'Sheryl Sandberg', 'Facebook Campus N. 222', 'Menlo Park',
+'MP', '81 98888-8888', '81 98888-8888');
+insert into vendedor values(1, 'Isaac Newton');
+insert into vendedor values(2, 'Albert Einstein');
+insert into vendedor values(3, 'Marie Curie');
+insert into vendedor values(6, 'Stephen Hawking');
+insert into vendedor values(5, 'Charles Darwin');
+insert into vendedor values(5, 'Nikola Tesla');
+insert into vendedor values(7, 'Galileo Galilei');
+insert into vendedor values(8, 'Leonardo da Vinci');
+insert into vendedor values(9, 'Ada Lovelace');
+insert into vendedor values(10, 'Alan Turing');
+
+insert into fabrica values(1, 'Ninjutsu Ltda.', 'Rua das Sombras N. 123', 'Esconderijo dos
+Ninjas', 'EN', '81 98123-1234');
+insert into fabrica values(2, 'Chapéu de Palha Indústrias', 'Rua dos Piratas N. 17', 'Vila
+Fuchsia', 'VF', '81 98 321-3210');
+insert into fabrica values(3, 'Forjas da Alma', 'Rua da Alma N. 1', 'Alma City', 'AC', '81
+98456-4567');
+insert into fabrica values(4, 'Metal Alquímico S.A.', 'Rua da Transmutação N. 240', 'Cidade
+dos Alquimistas', 'CA', '81 98654-6543');
+insert into fabrica values(5, 'Arsenal dos Caçadores', 'Rua da Caça N. 137', 'Quartel General
+dos Caçadores', 'QC', '81 98999-9999');
+insert into fabrica values(6, 'Forja das Fadas', 'Rua das Fadas N. 7', 'Vila Mágica', 'VM', '81
+98098-0987');
+
+insert into fabrica values(7, 'Estaleiro dos Piratas', 'Rua dos Piratas N. 1', 'Cidade Marítima',
+'CM', '81 98567-8459');
+insert into fabrica values(8, 'Armas das Feras', 'Rua das Feras N. 13', 'Cidade Selvagem',
+'CS', '81 98105-3845');
+insert into fabrica values(9, Mundo Mágico', 'Rua Mágica N. 42', 'Cidade dos Feiticeiros',
+'CF', '81 98053-5375');
+insert into fabrica values(10, 'Feitiçaria Ltda.', 'Rua dos Feiticeiros N. 93', 'Vila Mágica', 'VM',
+'81 98573-8475');
+
+insert into transportadora values(1, 'Correio dos Shinigamis', 'Rua da Soul Society N. 486',
+'Soul Society', 'SS');
+insert into transportadora values(2, 'Guilda dos Caçadores', 'Rua dos Caçadores N. 245',
+'Magnolia', 'MG');
+insert into transportadora values(3, 'Porto de Maré', 'Rua dos Piratas N. 121', 'Vila Fuchsia',
+'VF');
+insert into transportadora values(4, 'Nuvens Velozes', 'Rua dos Céus N. 567', 'Sky Island',
+'SI');
+insert into transportadora values(5, 'Dragão Express', 'Montanha dos Dragões N. 123',
+'Dragon's Den', 'DD');
+insert into transportadora values(6, 'Táxi do Céu', 'Céu Estrelado N. 456', 'Céu', 'CE');
+insert into transportadora values(7, 'Agência de Entrega Espacial', 'Lua Alpha N. 789',
+'Sistema Solar', 'SS');
+insert into transportadora values(8, 'Transportadora Galáctica', 'Estrela Polar N. 321', 'Via
+Láctea', 'VL');
+insert into transportadora values(9, 'Transportadora Ninja', 'Aldeia Oculta da Folha N. 567',
+'Konoha', 'KN');
+insert into transportadora values(10, 'Caminho da Aventura', 'Trilha do Herói N. 890',
+'Várias', 'VV');
+insert into condpagto values(1, 'PIX');
+insert into condpagto values(2, 'Boleto bancário');
+insert into condpagto values(3, 'Transferência bancária');
+insert into condpagto values(4, 'Cartão de crédito/uma parcela');
+insert into condpagto values(5, 'Cheque');
+insert into condpagto values(6, 'A vista');
+insert into condpagto values(7, 'Crédito com parcelas sem juros');
+insert into condpagto values(8, 'Crédito com parcelas com juros');
+insert into condpagto values(9, 'PayPal');
+insert into condpagto values(10, 'Depósito em conta');
+insert into produto values(1, '32', 'Varinha Mágica de Sabugueiro', 'UN', 1, 1);
+insert into produto values(2, '54', 'Cerveja Amanteigada', 'UN', 2, 2);
+insert into produto values(3, '67', 'Cálice de Fogo', 'UN', 3, 3);
+insert into produto values(4, '50', 'Pomo de Ouro', 'UN', 4, 4);
+insert into produto values(5, '13', 'Vira-Tempo', 'UN', 5, 5);
+insert into produto values(6, '45', 'Capa da Invisibilidade', 'UN', 6, 6);
+
+insert into produto values(7, '1', 'Pedra Filosofal', 'UN', 7, 7);
+insert into produto values(8, '14', 'Berrador', 'UN', 8, 8);
+insert into produto values(9, '98', 'Varinha das Varinhas', 'UN', 9, 9);
+insert into produto values(10, '64', 'Poção Polissuco', 'UN', 10, 10);
